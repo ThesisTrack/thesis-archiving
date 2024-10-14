25633888d0a88986b170d1944f2e767323faff8b
LuisBulatao <bluisjoshua@gmail.com>
2024-10-14 17:07:07 +0800

    Login Page

---
+++ src/components/homebackgroundimg.js
@@ -0,0 +1,14 @@
+
+export  const backgroundImage  = {
+    backgroundImage: 'url(new_era_university_qc_main_cover.png)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    color: 'white',
+    margin: 0,
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    filter: 'blur(8px)'
+};
