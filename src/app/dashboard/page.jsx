25c0f5bf6d58fce925a6a4eb57eb397c41374003
LuisBulatao <bluisjoshua@gmail.com>
2024-10-21 22:35:02 +0800

    Update Schema

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,17 @@
+import { getServerAuthSession } from "@/server/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+const Dashboard = () => {
+  const session = getServerAuthSession();
+
+  console.log(session)
+
+  if (!session) {
+    redirect('/login')
+  }
+
+  return <h1>Helloasd</h1>;
+};
+
+export default Dashboard;
