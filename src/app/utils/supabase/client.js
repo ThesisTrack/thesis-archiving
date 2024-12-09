a53c850f9f79c4e132c35eae85353045a907f773
LuisBulatao <bluisjoshua@gmail.com>
2024-12-10 02:05:58 +0800

    Update and Implemented

---
+++ src/app/utils/supabase/client.js
@@ -0,0 +1,8 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+export function createClient() {
+  const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+  return createBrowserClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
