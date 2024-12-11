e071152ef9f55cdc79f70131cab5556e240abc9c
Keanu Paul <yugen.keanupaul@gmail.com>
2024-12-12 02:35:58 +0800

    browsetheses

---
+++ src/app/browsetheses/page.js
@@ -0,0 +1,15 @@
+'use client'
+
+import { NavigationBar } from "@/components/Common/NavigationBar"
+import { Footer } from "@/components/Common/Footer"
+
+export default function Home(){
+    return(
+
+        <div>
+            <NavigationBar/>
+            DITO PO ILALALAGAY UNG TABLE, THANKYOU BERIMATSUU
+            <Footer/>
+        </div>
+    )
+}
